05b39a7fe0cc27f44f9b62547069a5290a638a52
Dan Bergh Johnsson <dan.bergh.johnsson@omegapoint.se>
2014-04-21 19:33:20 +0200

    Changed computiation to check whether n is a prime, to give a slightly flatter computation effort over the list

--- run-java-on-ec2/java-on-ec2/src/main/java/danbjson/tmp/App.java
+++ run-java-on-ec2/java-on-ec2/src/main/java/danbjson/tmp/App.java
@@ -11,12 +11,40 @@ public class App
 {
     public static void main( String[] args )
     {
+        long start = System.currentTimeMillis();
         List<Integer> lotsofints = new ArrayList<Integer>();
-        for(int i=0; i < 1000000; i ++) {
+        for(int i=2; i < 300000; i ++) {
             lotsofints.add(i);
         }
-        lotsofints.stream().parallel().forEach(
-                (n) -> System.out.println(String.format("Thread-exec %d %s", n, Thread.currentThread().getName()))
+        lotsofints.stream().forEach(
+                (n) -> {
+                    boolean prime = isPrime(n);
+                    System.out.println(String.format("Thread-exec %d %b %s", n, prime, Thread.currentThread().getName()));
+                }
         );
+        long stop = System.currentTimeMillis();
+        System.out.println(String.format("Total time (s): %d", (stop-start) / 1000));
+    }
+
+
+    private static boolean isPrime(long largenumber) {
+        boolean hasDivisor = false;
+        for(int n=2; n<largenumber && !hasDivisor; n++ ) {
+          if(largenumber % n == 0) hasDivisor = true;
+        }
+        return !hasDivisor;
+
+    }
+
+    private static long fib(int n) {
+        long result;
+        switch (n) {
+            case 0 : result = 1; break;
+            case 1 : result = 1; break;
+            default:
+                result = fib(n - 2) + fib(n - 1);
+                break;
+        }
+        return result;
     }
 }
